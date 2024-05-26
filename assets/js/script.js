a851f90c2c65770bd0f00f3b5f2ac8667fa8b138
brxwnsugxr <jsdev.mas@gmail.com>
2024-05-26 15:55:39 +1000

    tempAPI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,62 @@
 let cityInput = document.getElementById('city_input');
 searchBtn = document.getElementById('searchBtn');
-api_key = '9351ee22b0917e5901e60be617cf6f2b';
+(api_key = '9351ee22b0917e5901e60be617cf6f2b'),
+  (currentWeatherCard = document.querySelectorAll('.weather-left .card')[0]);
+
+function getWeatherDetails(name, lat, lon, country, state) {
+  let FORECAST_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`;
+  let WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,
+    days = [
+      'Sunday',
+      'Tuesday',
+      'Wednesday',
+      'thursday',
+      'fridday',
+      'saturday',
+    ],
+    months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'June',
+      'July',
+      'Augs',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+  fetch(WEATHER_API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      let date = new Date();
+      currentWeatherCard.innerHTML = `
+      <div class="current-weather">
+      <div class="details">
+        <p>Now</p>
+        <h2>${(data.main.temp - 273.15).toFixed(2)}&deg;C</h2>
+        <p>_____</p>
+      </div>
+      <div class="weather-icon">
+        <img
+          src="https://openweathermap.org/img/wn/04d@2x.png"
+          alt=""
+        />
+      </div>
+    </div>
+    <hr />
+    <div class="card-footer">
+      <p><i class="fa-light fa-calander"></i>_____</p>
+      <p><i class="fa-light fa-location-dot"></i>_____</p>
+    </div>
+      `;
+    })
+    .catch(() => {
+      alert('Failed to fetch current weather');
+    });
+}
 
 function getCityCoordinates() {
   let cityName = cityInput.value.trim();
